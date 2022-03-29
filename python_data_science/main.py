eb80556ee1ecd6190d35180bf535cc1cd874326e
Orlando J Betancourth <orlando.betancourth@gmail.com>
2022-03-28 19:57:34 -0600

    20220328 matplotlib barchart

--- python_data_science/main.py
+++ python_data_science/main.py
@@ -3,9 +3,15 @@ from unittest import skip
 # python -m pip install csv
 # pip install csv
 
+import matplotlib.pyplot as plt
+# python -m pip install matplotlib
+# pip intall matplotlib
+
 with open('data/Machine_readable_file_bdc_sf_2021_q4.csv') as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
   dic_grouping = dict()
+  axs_value = list()
+  lbls_value = list()
   is_first_line = True
   for csv_line in csv_reader:
     if is_first_line:
@@ -22,4 +28,12 @@ with open('data/Machine_readable_file_bdc_sf_2021_q4.csv') as csv_file:
     else:
       dic_grouping[series] = {"series": series, "value": valor, "count": 1}
   for data in dic_grouping:
-    print(dic_grouping[data])
+    axs_value.append(dic_grouping[data]["value"]/1000)
+    lbls_value.append(dic_grouping[data]["series"][0:10])
+    # print(dic_grouping[data])
+
+plt.bar(range(0,len(axs_value)), axs_value)
+plt.xticks(range(0,len(axs_value)), lbls_value, rotation=45)
+plt.ylabel('Series')
+plt.title('Series de Ingresos')
+plt.show()
